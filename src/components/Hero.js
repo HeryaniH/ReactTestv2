b0a34146638589b9045ce30f6016c79904d8705a
HeryaniH <86170048+HeryaniH@users.noreply.github.com>
2021-06-28 17:57:43 +0700

    versionawal

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Image from '../assets/bg-img1.jpeg';
+import { MdKeyboardArrowRight } from 'react-icons/md'
 
 const Section = styled.section`
     background-image: url(${Image});
@@ -15,10 +16,42 @@ const Content = styled.div`
     height: 100px;
 `;
 
-const Left = styled.div``;
-const Tittle = styled.p``;
-const Desc = styled.p``;
-const Button = styled.a``;
+const Left = styled.div`
+padding-left: 10em;
+padding-top: 10em;
+`;
+
+const Tittle = styled.p`
+    font-size: 55px;
+    color: #04050a;
+    font-weight: 400;
+`;
+
+const Desc = styled.p`
+    width: 472px;
+    font-size: 20px;
+    color: #9ea0ac;
+    line-height: 30px;
+    margin-top: 58px;
+`;
+
+const Button = styled.a`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 18px;
+    margin-top: 58px;
+    width: 371px;
+    height: 71px;
+    line-height: 71px;
+    font-size: 22px;
+    text-align: center;
+    color: #fff;
+    cursor: pointer;
+    background: linear-gradient(90deg, #0546d6, #3f89fc);
+    text-decoration: none;
+    box-shadow: 0 15px 14px rgb(0 42 177 / 12%);
+`;
 
 const Hero= () => {
     return (
@@ -36,7 +69,8 @@ const Hero= () => {
                     </Desc>
                     <Button href='/'
                     target='_blank'> 
-                        <span>Claim your free stocks now</span>
+                        <span>Join Now!</span>
+                        <MdKeyboardArrowRight />
                     </Button>
                </Left>
            </Content>
